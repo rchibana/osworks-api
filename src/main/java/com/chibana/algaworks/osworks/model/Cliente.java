510c1b1faa3c95fab5782e08a1ce35a0bcbb0fb3
Rodrigo Chibana <rodrigo.chibana@gmail.com>
2020-04-26 16:02:37 -0300

    Created migrations

--- src/main/java/com/chibana/algaworks/osworks/model/Cliente.java
+++ src/main/java/com/chibana/algaworks/osworks/model/Cliente.java
@@ -2,17 +2,38 @@ package com.chibana.algaworks.osworks.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Created by Rodrigo Chibana
  * Date: 26/04/2020
  **/
 
 @Data
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Email
+    @Size(max=255)
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "fone")
     private String telefone;
 
 }
